63a0cbceb03dceafe4a49ede33ac1737dc549a86
tourdedave <dhaeffer@gmail.com>
2020-12-11 12:15:53 -0500

    increase timeout

--- wdio.conf.js
+++ wdio.conf.js
@@ -129,7 +129,7 @@ exports.config = {
   // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: 'bdd',
-    timeout: 60000
+    timeout: 180000
   },
   eyes: {
     apiKey: process.env.APPLITOOLS_API_KEY,
